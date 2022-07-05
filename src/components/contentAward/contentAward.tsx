50a366884baa376ae0a463a6060b8edaa8c93e67
kt5680608 <gns7893@naver.com>
2022-07-05 14:33:24 +0900

    refactor: 스타일링 코드 분리 및 카운팅 애니메이션 보완

--- src/components/contentAward/contentAward.tsx
+++ src/components/contentAward/contentAward.tsx
@@ -1,14 +1,7 @@
-import styled from 'styled-components'
-
 import { Box, Image } from '../../styles/globalStyles'
 
-const AwardInfo = styled.div`
-  font-size: 14px;
-  color: var(--g-color-grey100);
-  font-weight: bold;
-  margin-left: 8px;
-  line-height: 22px;
-`
+import { AwardInfo } from './styles'
+
 function ContentAward() {
   return (
     <Box
